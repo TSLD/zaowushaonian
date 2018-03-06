2516d4ecc898658d26000dcc5f0e926f9fc1bc54
TSLD <fuchao199@outlook.com>
2018-03-06 17:18:59 +0800

    app3
    e

---
+++ zaowushaonian.6.18/review/includes/include-1520327906344.js
@@ -0,0 +1,6 @@
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/_jim/javascript/jim-min.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/scenarios/function-jim-links1520327906344.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/_jim/javascript/mobile/ios/ios8/iphone/jim-iphone-min.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/scroll-1520327906344.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/prototype-1520327906344.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/scale-1520327906344.js'></script>");
